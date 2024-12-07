a5e73458c493b4c2bc3ac84c72fd4b40dad6e6e4
julia <julia.zhyvylo@asila.mk>
2024-12-07 20:15:18 +0100

    add task 2 logic

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,4 +1,44 @@
-import iziToast from "izitoast";
+import iziToast from 'izitoast';
 
-import "izitoast/dist/css/iziToast.min.css";
+import 'izitoast/dist/css/iziToast.min.css';
 
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', e => handleFormSubmit(e));
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+
+  const delayMs = e.target.elements.delay.value;
+  const state = e.target.elements.state.value;
+
+  makePromise(delayMs, state)
+    .then(value =>
+      iziToast.show({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${value}ms`,
+        position: 'topRight',
+        color: 'green',
+      })
+    )
+    .catch(error =>
+      iziToast.show({
+        title: 'Error',
+        message: `❌ Rejected promise in ${error}ms`,
+        position: 'topRight',
+        color: 'red',
+      })
+    );
+}
+
+function makePromise(delayMs, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delayMs);
+      }
+      if (state === 'rejected') {
+        reject(delayMs);
+      }
+    }, delayMs);
+  });
+}
